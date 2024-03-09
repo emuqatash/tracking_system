a8066e00f80071f01c4180b9e274ed0bf3478f37
emuqatash <emuqatash@gmail.com>
2024-03-09 14:50:42 -0600

    master fix S3 for file uploads

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,14 +44,15 @@ return [
             'throw' => false,
         ],
 
+
         's3' => [
             'driver' => 's3',
-            'key' => env('DO_ACCESS_KEY_ID'),
-            'secret' => env('DO_SECRET_ACCESS_KEY'),
-            'region' => env('DO_DEFAULT_REGION'),
-            'bucket' => env('DO_BUCKET'),
-            'url' => env('DO_URL'),
-            'endpoint' => env('DO_ENDPOINT'),
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
